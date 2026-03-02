1fac5d9525057e9a2d1a42be3ac0892c2ce73693
Naveenapolineni05 <naveenapolineni05@gmail.com>
2026-03-02 17:17:51 -0600

    Implemented users crud operations

---
+++ src/main/java/com/example/foodcalorietracker/models/Users.java
@@ -0,0 +1,53 @@
+package com.example.foodcalorietracker.models;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String password;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Meals> meals;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Goals goals;
+
+    public Users() {
+    }
+
+    public Users(String email, int id, String password) {
+        this.email = email;
+        this.id = id;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+
