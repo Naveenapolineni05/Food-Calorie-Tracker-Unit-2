f644979e9449aefa2fc842dd54964183c3197f4f
Naveenapolineni05 <naveenapolineni05@gmail.com>
2026-03-02 03:19:34 -0600

    Updated application.properties

---
+++ src/main/java/com/example/foodcalorietracker/FoodcalorietrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodcalorietracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodcalorietrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodcalorietrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foodcalorietracker/FoodcalorietrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodcalorietracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcalorietrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
